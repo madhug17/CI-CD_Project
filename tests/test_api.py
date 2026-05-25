689e7bf971d687d2ff84b6b195b0282ea759b090
G Madhu Goud <goundlamadhug@gmail.com>
2026-05-25 13:38:58 +0530

    updates the test case

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,6 @@
+import sys 
+import os 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 import pytest 
 from fastapi.testclient import TestClient
 from app.main import app
@@ -7,4 +10,3 @@ def test_predict_endpoint():
     responce = client.post("/predict",json={"text":"nvidia is great!"})
     assert responce.status_code == 200
     assert "prediction" in responce.json()
-    
